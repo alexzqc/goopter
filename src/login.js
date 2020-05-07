aa6f96d3f50c0e72e1d92881429feb23229c0c51
alexzqc <alexziqic@gmail.com>
2020-05-07 10:28:57 -0700

    finish route and bootstrap

---
+++ src/login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Redirect } from 'react-router-dom';
+
+class login extends React.Component {
+
+render(){
+return(
+<div className="container">
+    <p>This is login page.</p>
+</div>
+);
+
+    }
+
+}
+
+export default login;
